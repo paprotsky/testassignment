0d2a807de21bb1d32c8800ad538160ba9cbae2ee
paprotsky <roman@paprotsky.com>
2015-03-10 11:26:39 +0200

    footer

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
       <link rel="shortcut icon" href="img/title.png">
       <title>Test Assignment</title>
 
+      <script src="js/jquery-2.1.1.min.js"></script>
+
       <link href="css/bootstrap.min.css" rel="stylesheet">
       <link href="font-awesome-4.3.0/css/font-awesome.css" rel="stylesheet">
    </head> 
@@ -21,9 +23,9 @@
          <div class="container">
             <div class="pull-right"><br>
                <?php
-                  $client_id = '911744139194-vdvvodha06dsj9fbg81g3s1osii2ccd0.apps.googleusercontent.com'; // Client ID
-                  $client_secret = 'c2taLQJXjm3NoyRu9AsXWgHZ'; // Client secret
-                  $redirect_uri = 'http://testassignment.zaladmin.com/oauth2callback.php'; // Redirect URI
+                  $client_id = '911744139194-dfg34b5nglna16c3t1f7k40oq4s5a8dd.apps.googleusercontent.com'; // Client ID
+                  $client_secret = '8oVBldxeFVrpJTI15RLGrbTm'; // Client secret
+                  $redirect_uri = 'http://testassignment.com/oauth2callback.php'; // Redirect URI
 
                   $url = 'https://accounts.google.com/o/oauth2/auth';
 
@@ -38,20 +40,33 @@
                   ?>
             </div>
          </div>
+         <footer>
+            <p>&copy; roman@paprotsky.com</p>
+         </footer>
       </div>
       <script src="js/bootstrap.min.js"></script>
-      <script src="js/jquery-2.1.1.min.js"></script>
+      <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
    </body>
 </html>
 <style type="text/css">
    .googleBtn{
       color: #fff;
       background-color: #dd4b39;
-      margin-top: -13px;
+      margin-top: -15px;
    }
    body {
-    background: url(img/map.jpg);
+    background: url(img/map.png);
     background-size: 100% ;
     background-repeat: no-repeat;
+   }
+   footer{
+   /* background-color: #111111;*/
+       /*background-color: rgba(0,0,0,0.4);*/
+       color: #5a5a5a;
+      bottom: 0;
+   /* box-shadow: 0 -1px 2px rgba(0,0,0,0.4);*/
+   /* height: 45px;*/
+      left: 10px;
+      position: fixed;
    }
 </style>
